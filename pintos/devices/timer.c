06c23358ce8a6c45b097353cc2d3d4abb905b6cd
Edy Cocon <edycocon@galileo.edu>
2021-04-25 16:42:21 -0700

    Se agrega correcion para el negative

--- pintos/devices/timer.c
+++ pintos/devices/timer.c
@@ -89,12 +89,15 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
+  if(ticks < 0){
+    ticks = ticks*-1;
+  } 
+
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
-    
+ 
+
   insert_on_waiting_list(ticks);
 }
 
